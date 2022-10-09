6fa3d26db72e0ab80613a3336db3567c4897dc53
parixscm <parixscm@gmail.com>
2022-10-09 17:24:04 +0900

    Add buttons to Todo component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,12 +1,50 @@
-import { ITodo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { ITodo, todosState } from "../atoms";
 
-function Todo({ text }: ITodo) {
+function Todo({ text, category, id }: ITodo) {
+  const setTodos = useSetRecoilState(todosState);
+  const onClick = (newCategory: ITodo["category"]) => {
+    setTodos((oldTodos) => {
+      const targetIdx = oldTodos.findIndex((todo) => todo.id === id);
+      const newTodo = { text, id, category: newCategory };
+      return [
+        ...oldTodos.slice(0, targetIdx),
+        newTodo,
+        ...oldTodos.slice(targetIdx + 1),
+      ];
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>TODO</button>
-      <button>DOING</button>
-      <button>DONE</button>
+      {category !== "TODO" && (
+        <button
+          onClick={() => {
+            onClick("TODO");
+          }}
+        >
+          TODO
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button
+          onClick={() => {
+            onClick("DOING");
+          }}
+        >
+          DOING
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button
+          onClick={() => {
+            onClick("DONE");
+          }}
+        >
+          DONE
+        </button>
+      )}
     </li>
   );
 }
